d829b4462c97e0aeab4a548c8103317e894c363d
yztb97dd4j-crypto <yztb97dd4j@privaterelay.appleid.com>
2026-02-01 04:21:55 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Pookiee",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
